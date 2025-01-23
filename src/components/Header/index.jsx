dcaa5179f9864a5498923fbb1d62e6bbc066b039
Jéssica Ferreira <carolina.gonzalez.alvarenga@gmail.com>
2025-01-22 23:52:05 -0300

    Revert "feat: header component created"
    This reverts commit 579ea06652c8837f739b8a9c2ef9c16c458c4e20.

--- src/components/Header/index.jsx
+++
@@ -1,8 +0,0 @@
-import "./index.css";
-import ninjaLogo from '../../assets/ninjaone.svg'
-
-export const Header = () => (
-	<div className='header'>
-		<img className="header__logo" src={ninjaLogo} alt="Logo" />
-	</div>
-);
